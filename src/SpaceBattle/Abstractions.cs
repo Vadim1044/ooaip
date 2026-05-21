9ea8825ef7f3ee6cea576ccb000c16badfe73810
Vadim1044 <vadimvase@gmail.com>
2026-05-21 22:52:49 +0600

    kek

--- src/SpaceBattle/Abstractions.cs
+++ src/SpaceBattle/Abstractions.cs
@@ -4,25 +4,3 @@ public interface ICommand
 {
     void Execute();
 }
-
-public interface ICommandReceiver
-{
-    void Receive(ICommand command);
-}
-
-public interface ICommandInjectable
-{
-    void Inject(ICommand command);
-}
-
-public interface IMovingObject
-{
-    Vector Position { get; set; }
-    Vector Velocity { get; }
-}
-
-public interface IRotatingObject
-{
-    Angle Angle { get; set; }
-    Angle AngularVelocity { get; }
-}
